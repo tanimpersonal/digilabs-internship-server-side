94730da4c5ab2c19c4c6e059a07b06f8840bdaf8
tanimpersonal <tashintanim2019@gmail.com>
2022-08-03 19:58:04 +0600

    mongo parsed

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const uri =
+  "mongodb+srv://dbuser1:89NuSGyMNpr6nn4o@cluster0.755op.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const noticeCollection = client
+      .db("digilabs-internship")
+      .collection("notice");
 
+    app.get("/notices", async (req, res) => {
+      const query = {};
+      const cursor = noticeCollection.find(query);
+      const notices = await cursor.toArray();
+      res.send(notices);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });

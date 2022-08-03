e4c7deac7464c955ff930244f516535670dfed73
tanimpersonal <tashintanim2019@gmail.com>
2022-08-03 23:39:55 +0600

    gallery added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 const uri =
   "mongodb+srv://dbuser1:89NuSGyMNpr6nn4o@cluster0.755op.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -15,12 +18,26 @@ async function run() {
     const noticeCollection = client
       .db("digilabs-internship")
       .collection("notice");
-
+    const imageCollection = client
+      .db("digilabs-internship")
+      .collection("pictures");
     app.get("/notices", async (req, res) => {
       const query = {};
       const cursor = noticeCollection.find(query);
       const notices = await cursor.toArray();
       res.send(notices);
+    });
+    app.get("/picture", async (req, res) => {
+      const query = {};
+      const cursor = imageCollection.find(query);
+      const images = await cursor.toArray();
+      res.send(images);
+    });
+    app.post("/picture", async (req, res) => {
+      const imageUrl = req.body.displayUrl;
+      console.log(imageUrl);
+      const query = { imageUrl };
+      const result = await imageCollection.insertOne(query);
     });
   } finally {
   }
